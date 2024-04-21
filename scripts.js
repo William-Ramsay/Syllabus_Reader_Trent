78de375245ac89bf5668c4be83d0f5795dbcd82c
William Ramsay <williamramsay@trentu.ca>
2024-04-20 21:36:37 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('DOMContentLoaded', e=>{
+
+
+
+    let body = document.querySelector('body');
+    let input = document.querySelector('input');
+    let div = document.querySelector('div');
+    let result;
+    const reader = new FileReader();
+
+
+    input.addEventListener('change', e =>{
+        let file = input.files[0];
+        
+      
+        reader.readAsText(file);
+        reader.addEventListener('load', e =>{
+            result = reader.result;
+            console.log(result)
+        })
+
+    console.log('complete')
+
+ 
+    })
+
+
+    
+
+
+
+
+
+
+})//End of DOMContentLoaded
